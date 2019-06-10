0dadc1d1851d7fbf68adab381df168634ac6967e
Andreas Husch <27815260+adhusch@users.noreply.github.com>
2019-06-10 09:29:38 +0200

    PW, D Model (at 3V)

---
+++ GeneralHeuristic.m
@@ -0,0 +1,40 @@
+%% Examples on the "General Heuristic" of 0.2V/mm as proposed in the literature
+%
+%  Andreas Husch, University of Luxembourg, 2019
+%  andreas.husch(at)uni.lu
+%
+load('astrom_table3_3v.mat'); % loads PW, D, T for 3V 
+[activation_model_3v, gof] =  fitModel(PW, D, T);
+
+%% Hightlight 0.2V contour in 3D 
+figure
+h = plot( activation_model_3v, [PW, D], T ,'Xlim', [10 240], 'YLim', [1 8]);
+hold on
+vd = plot3(60,7.5,0.064, '*r');
+
+
+title('Axonal E-Field Activation Threshold for (PW,D) at 3V')
+
+% Label axes
+xlabel( 'PW [\mus]', 'Interpreter', 'Tex' );
+ylabel( 'D [\mum]', 'Interpreter', 'Tex');
+zlabel( 'T [V/mm]', 'Interpreter', 'Tex' );
+grid on
+view( 157.2, 26.3 );
+caxis([0.0369 2])
+
+hold on
+x = linspace(10,240);
+y = linspace(1,8);
+[X,Y] = meshgrid(x,y);
+Z = activation_model_3v(X,Y);
+[~,hc] = contour3(X,Y,Z, [0.2 0.2], 'r', 'LineWidth', 2);
+
+legend( [h; vd; hc], 'Model', 'Data', 'Validation Data', 'Proposed General Heuristics of 0.2V/mm', 'Location', 'NorthEast', 'Interpreter', 'none' );
+
+f = gcf;
+f.Color = 'w';
+a = gca;
+a.XTick = 30:30:270;
+
+
---
+++ PlotVTAsForDifferentPWorD.m
@@ -0,0 +1,58 @@
+nii = NiftiModSPM('/Users/andreas/Cloud Drive/ShanghaiLux/ADD/ADD_Yan_Wei_300039529/YanWei/stimulations/20190515172705/vat_efield_left.nii',1);
+%%
+figure
+PW = 90;
+Ds = 2:1:6;
+cmap = lines(length(Ds));
+h = {};
+legendStr = {};
+for i = 1:length(Ds)
+    s = isosurface(nii.img > activation_model_3v(PW,Ds(i))*1000);
+    h{end+1} = patch(s, 'EdgeColor', 'none', 'FaceColor', cmap(i,:), 'FaceAlpha', 0.3)
+    legendStr{end+1} = ['PW = ' num2str(PW) ' Diameter = ' num2str(Ds(i)) ];
+end
+
+legend(legendStr)
+title('Effect of different axon diamters')
+ylim([60 90])
+
+
+%%
+figure
+PW = 30:30:120;
+Ds = 3;
+cmap = lines(length(PW));
+h = {};
+legendStr = {};
+for i = 1:length(PW)
+    s = isosurface(nii.img > activation_model_3v(PW(i),Ds)*1000);
+    h{end+1} = patch(s, 'EdgeColor', 'none', 'FaceColor', cmap(i,:), 'FaceAlpha', 0.3)
+    legendStr{end+1} = ['PW = ' num2str(PW(i)) ' Diameter = ' num2str(Ds) ];
+end
+
+legend(legendStr)
+title('Effect of different PWs for constant diamter')
+ylim([60 90])
+
+
+%%
+figure
+PW =[30 60];
+Ds = [2 5.7];
+cmap = lines(length(PW)*length(Ds));
+h = {};
+legendStr = {};
+c=1;
+for i = 1:length(PW)
+    for j = 1:length(Ds)        
+        s = isosurface(nii.img > activation_model_3v(PW(i),Ds(j))*1000);
+        h{end+1} = patch(s, 'EdgeColor', 'none', 'FaceColor', cmap(c,:), 'FaceAlpha', 0.3)
+        legendStr{end+1} = ['PW = ' num2str(PW(i)) ' Diameter = ' num2str(Ds(j)) ];
+        c=c+1;
+        
+    end
+end
+
+legend(legendStr)
+title('Thin vs Thick Axon at Short vs Long PW')
+ylim([60 90])
